7e15008d9e8530605c1737d735ea4b47240f12e9
carlylezqy <carlylezqy@qq.com>
2023-03-17 11:14:05 +0900

    update

--- OpenMMLab/MMCV_Enhanced/model.py
+++
@@ -1,6 +0,0 @@
-from mmcv.utils import Registry, build_from_cfg
-MODELS = Registry('models')
-
-@MODELS.register_module()
-class ResNet:
-    pass
--- OpenMMLab/MMEngine_Enhanced/mmcls_run.py
+++ OpenMMLab/MMEngine_Enhanced/mmcls_run.py
@@ -1,6 +1,7 @@
 import os; os.chdir(os.path.dirname(__file__))
 from mmengine import Config
 from mmengine.runner import Runner
+from mmengine.hub import get_model
 from mmcls.models import build_classifier
         
 if __name__ == '__main__':
@@ -9,7 +10,12 @@ if __name__ == '__main__':
     work_dir='/home/akiyo/sandbox/ResNetConfigTrain'
     
     train_dataloader = Runner.build_dataloader(config.train_dataloader)
+    
+    # Method 1
     model = build_classifier(config.model)
+
+    # Method 2
+    #model = get_model('mmcls::configs/resnet/resnet50_8xb16_cifar10.py')
 
     runner = Runner(
         model, work_dir, 
